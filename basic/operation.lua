8751a006814991e3f216c619f77c08678b863ef5
devjun10 <devjun1023@gmail.com>
2024-05-27 16:24:34 +0900

    산술, 논리, 비교 연산자

---
+++ basic/operation.lua
@@ -0,0 +1,22 @@
+local x = 3
+local y = 4
+
+-- 산술 연산자
+local sum = x+y
+local sub = x-y
+local mul = x*y
+local div = x/y
+
+-- 논리 연산자
+local andResult = (x>y) and (b>0)
+local orResult = (x>y) or (y>0)
+local notResult = not(x>y)
+
+-- 비교 연산자 
+local isEqual = (x==y)
+local isNotEqual = (x ~= y)
+local isGreater = (x>y)
+local isLesser = (x<y)
+local isGreaterOrEqual = (x>=y)
+local isLessrOrEqual = (x<=y)
+
