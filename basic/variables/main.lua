14ccba34fffac8116eab8da45587b2307d1fa39c
devjun10 <>
2024-09-10 12:14:00 +0900

    [CODE] Add variables code.

---
+++ basic/variables/main.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+a = 10
+M.number = 10
+M.text = "HELLO WORLD"
+M.flag = true
+M.noValue = nil
+M.pi = 3.141592
+M.person = { name = "John", age = 30 }
+M.array = { 1, 2, 3, 4, 5 }
+
+return M
